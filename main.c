27d5e1f0b6b779d467a192b23e377d8194939550
OneTruBlu <blueberrypie.20201@gmail.com>
2025-09-26 21:02:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
 #include "vendor\sokol\sokol_app.h"
 #include "vendor\sokol\sokol_gfx.h"
 #include "vendor\sokol\sokol_glue.h"
+#include "triangle_shader.h"
 
 static struct{
   sg_pass_action pass_action;
+  sg_bindings binding;
+  sg_pipeline pipeline;
 } state;
 
 void init(){
@@ -17,15 +20,37 @@ void init(){
     }
   );
 
+  float vertices[] = {
+    // x ,  y ,  z ,   r ,   g ,   b 
+    0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 
+    -0.5, -0.5f, 0.0f,1.0f, 1.0f, 0.0f,
+  };
+
+  state.binding.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+      .data = SG_RANGE(vertices),
+  });
+
+  state.pipeline = sg_make_pipeline(&(sg_pipeline_desc){
+    .shader = sg_make_shader(triangle_shader_desc(sg_query_backend())),
+    .layout = {.attrs = {
+      [ATTR_triangle_pos].format = SG_VERTEXFORMAT_FLOAT3,
+      [ATTR_triangle_color].format = SG_VERTEXFORMAT_FLOAT3,
+    }}
+  });
+
   state.pass_action = (sg_pass_action){
     .colors[0] = {.load_action = SG_LOADACTION_CLEAR, 
-    .clear_value = {0.4, 0.6, 1.0, 1.0}
+    .clear_value = {0.145, 0.161, 0.180, 1.0}
     }
   };
 }
 
 void frame(){
   sg_begin_pass(&(sg_pass){.action = state.pass_action, .swapchain = sglue_swapchain()});
+  sg_apply_pipeline(state.pipeline);
+  sg_apply_bindings(&state.binding);
+  sg_draw(0, 3, 1);
   sg_end_pass();
   sg_commit();
 }
@@ -44,6 +69,7 @@ int main() {
       .height = 720,
       .init_cb = init,
       .frame_cb = frame,
+      .window_title = "triangle shader",
       .cleanup_cb = cleanup,
       .event_cb = event,
   });
